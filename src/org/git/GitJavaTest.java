4992e5791cfb3cf6bd945a6d8265f2a8e7545e3d
shakthitest <testingstudyforme@gmail.com>
2025-08-01 13:52:21 +0530

    updated

--- src/org/git/GitJavaTest.java
+++ src/org/git/GitJavaTest.java
@@ -6,12 +6,17 @@ public class GitJavaTest {
 	    public void displayMessage() {
 	        System.out.println("Hello");
 	    }
-
+	    
+	    private void displayContent() {
+	    	System.out.println("Bye");
+			
+		}
 	    public static void main(String[] args) {
 	        
 	        GitJavaTest obj = new GitJavaTest();
 
 	        obj.displayMessage();
+	        obj.displayContent();
 	    }
 	}
 
